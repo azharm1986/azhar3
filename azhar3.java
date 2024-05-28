39adb178dd75d8c170fa48fbabb697bc857c24c6
Azharm1986 <azhar.mohammadi1393@gmail.com>
2024-05-28 10:38:13 +0330

    zahra

---
+++ azhar3.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class azhar3 {
+    public static void main(String[] args) {
+        long age = 10003122;
+        long height = 150L;
+        long BirthYear = 1986;
+        long max = Long.MAX_VALUE;
+        long overMax=999999999999999999L;
+
+        System.out.println(max);
+    }
+}
